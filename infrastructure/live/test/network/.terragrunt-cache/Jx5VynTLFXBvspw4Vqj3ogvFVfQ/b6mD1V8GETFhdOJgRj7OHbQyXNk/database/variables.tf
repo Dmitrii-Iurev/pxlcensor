e330ebf1fde0f89fa8dccbc730ef47d8325800b3
DmitriiIurevPXL <Dmitrii.Iurev@student.pxl.be>
2026-01-10 21:40:39 +0100

    Delete infrastructure/live/test/network/.terragrunt-cache/Jx5VynTLFXBvspw4Vqj3ogvFVfQ/b6mD1V8GETFhdOJgRj7OHbQyXNk directory

--- infrastructure/live/test/network/.terragrunt-cache/Jx5VynTLFXBvspw4Vqj3ogvFVfQ/b6mD1V8GETFhdOJgRj7OHbQyXNk/database/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "environment" { type = string }
-variable "region" { type = string }
-variable "vpc_id" { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "db_secret_arn" { type = string }
